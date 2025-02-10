a29d8cf20cfa38ee2cef78cfed6a6331d3c3a3c4
x <x@live.com>
2025-02-10 10:52:44 +0200

    add global styles

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,44 @@
+// src/styles/globalStyles.ts
+import { ActivityIndicator, StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  // Shared title style used across multiple pages
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 30,
+    textAlign: 'center',
+    color: '#333',
+  },
+  subTitle: {
+    fontSize: 24,
+    marginBottom: 30,
+    color: '#555',
+  },
+  header:
+  {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  // You can add more shared styles here
+  input: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    padding: 12,
+    marginBottom: 20,
+    backgroundColor: '#fff',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#f9f9f9',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  activityIndicator: {
+    height: '35%',
+  }
+});
